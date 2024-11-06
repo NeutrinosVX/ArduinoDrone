4516eb290da01d79d49f7e03eba729bd64761bbd
Isaac Ge <118192154+NeutrinosVX@users.noreply.github.com>
2024-11-06 22:13:06 +0800

    Update XF-01P.ino

--- XF-01P.ino
+++ XF-01P.ino
@@ -1,10 +1,22 @@
+#define RC_TYPE  1 //设备类型：遥控发射器=1，接收机=0
+#define NRF24L01_CE_PIN   8
+#define NRF24L01_CSN_PIN   10
+#define NRF24L01_MOSI_PIN   11
+#define NRF24L01_MISO_PIN   12
+#define NRF24L01_SCK_PIN   13
 #include <Kalman.h>
 #include <Wire.h>
 #include <Math.h>
 #include <TFMPI2C.h>
-#include <NRFLite.h>
+#include <printf.h>
+#include <RF24.h>
 #include <SPI.h>
 #include <PID_v1.h>
+#include <Servo.h>
+Servo Motor1;
+Servo Motor2;
+Servo Motor3;
+Servo Motor4;
 //the project started at 2023/11/9
 //later logs will be written as improvements are made
 float fRad2Deg = 57.295779513; //Rads to Degrees
@@ -22,16 +34,16 @@ uint8_t Strength=0;//Radar signal Strength
 const int altiTrig=8;
 const int altiecho=9;
 unsigned  int relalti=0;
+uint8_t Mission=1;
 Kalman kalmanRoll; //Roll filter
 Kalman kalmanPitch; //Pitch filter
-Kalman Kalmandist; //distance filter
-Kalman kalmanalt; 
-uint8_t frontleft=0;//PWMpins for motors
-uint8_t frontright=A1;
-uint8_t hindleft=A2;
-uint8_t hindright=A3;
+RF24 radio(NRF24L01_CE_PIN, NRF24L01_CSN_PIN);
+double Kp=2, Ki=5, Kd=1;
+double target, Input, Output;
+PID myPID(&Input, &Output, &target, Kp, Ki, Kd, DIRECT);
 /*void getAltidata()
 {
+  #Single Sampling 
   unsigned int duration=0;
   digitalWrite(altiTrig, LOW);  
 	delayMicroseconds(2);  
@@ -44,6 +56,68 @@ uint8_t hindright=A3;
   Serial.print(relalti);
   Serial.print("cm");
 }*/
+
+//要在一对无线电上使用不同的地址，让这些地址用于配对
+uint8_t address[][6] = { "1Node", "2Node" };// 0 uses address[0] to transmit, 1 uses address[1] to transmit
+
+//发送缓冲区
+uint8_t payload[sizeof(float)*8+4];// // only using 4 bytes for TX payloads
+
+//接收缓冲区
+uint8_t received[4] = {0,0,0,0};// // only using 4 bytes for ACK payloads
+
+uint16_t NRF24L01sender() {
+  // This device is a TX node
+
+  uint16_t ret = 0xFEEF;
+
+  //payload[0] = 0xFE;
+  //payload[1] = (byte)((value & 0xFF00)>>8);
+  //payload[2] = (byte)(value & 0xFF);
+  //payload[3] = 0xEF;
+
+  unsigned long start_timer = micros();                  // start the timer
+  bool report = radio.write(&payload, sizeof(payload));  // transmit & save the report
+  unsigned long end_timer = micros();                    // end the timer
+  if (report) {
+    Serial.print(F("Transmission successful! "));  // payload was delivered
+    Serial.print(F("Time to transmit = "));
+    Serial.print(end_timer - start_timer);  // print the timer result
+    Serial.print(F(" us. Sent: "));
+    for(int i=0;i<sizeof(payload);i++){
+  	Serial.print(payload[i],HEX);  // print incoming message
+  	Serial.print(' ');
+    }
+    Serial.println("");
+    uint8_t pipe;
+    if (radio.available(&pipe)) {  // 有ACK（确认收到）有效载荷吗？获取收到它的信道号
+
+  	radio.read(&received, sizeof(received));  // get incoming ACK payload
+  	Serial.print(F(" Recieved "));
+  	Serial.print(radio.getDynamicPayloadSize());  // print incoming payload size
+  	Serial.print(F(" bytes on pipe "));
+  	Serial.print(pipe);  // print pipe number that received the ACK
+  	Serial.print(F(": "));
+      for(int i=0;i<radio.getDynamicPayloadSize();i++){
+  	  Serial.print(received[i],HEX);  // print incoming message
+  	  Serial.print(' ');
+      }
+  	Serial.println("");
+
+    }
+     else {
+  	Serial.println(F(" Recieved: an empty ACK packet"));  // empty ACK packet received
+    }
+
+    ret = 0;
+
+  } 
+  else {
+    Serial.println(F("Transmission failed or timed out"));  // payload was not delivered
+  }
+
+  return ret;
+}
 void getAltidata()//the multisampling version, works stable. adding Kalman filtering method in future version
 {
   const int samp=4;// how many samples 
@@ -104,7 +178,6 @@ void ReadAccGyr(int *pVals)
   for(int i=0;i<nValCnt;++i)
   {
     pVals[i] = Wire.read() << 8 | Wire.read();
-  
   }
 }
 
@@ -159,8 +232,7 @@ void Rectify(int *pReadout, float *pRealVals)
     pRealVals[i] = (float)(pReadout[i] - calibData[i]) / 131.0f;
   }
 }
-
-void Get_LidarDatafromIIC(unsigned char address)
+int Get_LidarDatafromIIC(unsigned char address)
 {
   char i = 0; 
   unsigned char rx_buf[9] = {0}; 
@@ -192,16 +264,25 @@ void Get_LidarDatafromIIC(unsigned char address)
     {
       check_sum += rx_buf[i];
     }
-   if(rx_buf[0] == 0x59 && rx_buf[1] == 0x59 &&  rx_buf[8] == check_sum)
-   {
+    if(rx_buf[0] == 0x59 && rx_buf[1] == 0x59 &&  rx_buf[8] == check_sum)
+    {
       Serial.print("---------->");
       Serial.print("Distance=");
       dist=rx_buf[3]*256+rx_buf[2];
+      
       Serial.print(dist);//distance in int
       Serial.print(";");
       Strength=rx_buf[5]*256+rx_buf[4];
       Serial.print("Strength=");
       Serial.print(Strength);//strength in int
+      if(Strength>=30)
+      {
+        return dist;
+      }
+      else
+      {
+        return 0;
+      }
    }
    else
    {
@@ -253,8 +334,6 @@ void GetLidarVersionFromIIC(unsigned char address)
    {
       Serial.print("Check version error!");
    }
-
-    
   Serial.print("\r\n"); 
 }
 
@@ -349,23 +428,6 @@ void SaveLidarSetFromIIC(unsigned char address)
 
   Serial.print("\r\n"); 
 }
-void setup() 
-{
-
-  pinMode(LED_BUILTIN, OUTPUT);//LED, system starts
-  pinMode(altiTrig,OUTPUT);
-  pinMode(altiecho,INPUT);
-  pinMode(13,OUTPUT);
-  Serial.begin(115200); //初始化串口，指定波特率
-  Wire.begin(); //初始化Wire库
-  WriteMPUReg(0x6B, 0); //启动MPU6050设备,6B是启动电平发送
-  GetLidarVersionFromIIC(0x10);
-  SaveLidarSetFromIIC(0x10);//Save #10 Lidar`s set.
-  Calibration(); //执行校准
-  nLastTime = micros(); //记录当前时间
-
-}
-
 void setamplfiers()//made for higher speed or rotational motion environments
 {
   /*Wire.write(0x1C);
@@ -381,50 +443,183 @@ void setamplfiers()//made for higher speed or rotational motion environments
   Wire.write(acc_conf);
   Wire.endTransmission(true); //结束传输，true表示释放总线
 }
+void setup() 
+{
+  pinMode(LED_BUILTIN, OUTPUT);//LED, system starts
+  pinMode(altiTrig,OUTPUT);
+  pinMode(altiecho,INPUT);
+  pinMode(13,OUTPUT);
+  Serial.begin(115200); //初始化串口，指定波特率
+  Wire.begin(); //初始化Wire库
+  WriteMPUReg(0x6B, 0); //启动MPU6050设备,6B是启动电平发送
+  GetLidarVersionFromIIC(0x10);
+  SaveLidarSetFromIIC(0x10);//Save #10 Lidar`s set.
+  Calibration(); //执行校准
+  nLastTime = micros(); //记录当前时间
+  delay(500);
+  //初始化无线收发模块
+    // initialize the transceiver on the SPI bus
+    if (!radio.begin()) {
+      Serial.println(F("radio hardware is not responding!!"));
+      while (1) {}  // hold in infinite loop
+    }
+    
+    //当发射与接收节点非常接近的情况下运行时，将PA级别（耗电）设置为较低，以防止产生供电相关的问题
+    radio.setPALevel(RF24_PA_LOW);  // RF24_PA_MAX is default.
+
+    // 要使用ACK有效载荷，我们需要启用动态有效载荷长度（针对所有节点）
+    radio.enableDynamicPayloads();  // ACK payloads are dynamically sized
+
+    //默认情况下，确认数据包没有有效载荷。我们需要启用
+    //此功能用于所有节点（TX和RX）使用ACK有效载荷。
+    radio.enableAckPayload();
+
+    // set the TX address of the RX node into the TX pipe
+    radio.openWritingPipe(address[RC_TYPE]);  // always uses pipe 0
+
+    // set the RX address of the TX node into a RX pipe
+    radio.openReadingPipe(1, address[!RC_TYPE]);  // using pipe 1
+
+    // additional setup specific to the node's role
+    if (RC_TYPE) {
+      // setup the TX payload
+      radio.stopListening();                 // put radio in TX mode
+    } 
+    else {
+      // 设置ACK有效载荷并将第一个响应加载到FIFO
+
+      //memcpy(payload.message, "World ", 6);  // set the payload message
+      // load the payload for the first received transmission on pipe 0
+      radio.writeAckPayload(1, &payload, sizeof(payload));
+      radio.startListening();  // put radio in RX mode
+    }
+
+  //初始化MPU6050
+  WriteMPUReg(0x6B, 0);
+  Calibration();
+  nLastTime = micros();
+
+}
+void motor1(int p)
+{
+  while (Serial.available() == 0);
+  {
+    if(p < 1100 || p > 1900)
+    {
+      Serial.println("not valid");
+    }
+    else
+    {
+      Motor1.writeMicroseconds(p); // Send signal to ESC.
+    }
+  } 
+}
+void motor2()
+{
+  while (Serial.available() == 0);
+  {
+    int val = Serial.parseInt(); 
+    if(val < 1100 || val > 1900)
+    {
+      Serial.println("not valid");
+    }
+    else
+    {
+      Motor2.writeMicroseconds(val); // Send signal to ESC.
+    }
+  } 
+}
+void motor3()
+{
+  while (Serial.available() == 0);
+  {
+    int val = Serial.parseInt(); 
+    if(val < 1100 || val > 1900)
+    {
+      Serial.println("not valid");
+    }
+    else
+    {
+      Motor3.writeMicroseconds(val); // Send signal to ESC.
+    }
+  } 
+}
+void motor4()
+{
+  while (Serial.available() == 0);
+  {
+    int val = Serial.parseInt(); 
+    if(val < 1100 || val > 1900)
+    {
+      Serial.println("not valid");
+    }
+    else
+    {
+      Motor4.writeMicroseconds(val); // Send signal to ESC.
+    }
+  } 
+}
 void loop() 
 {
-  Get_LidarDatafromIIC(0x10);//Get #10 Lidar measurment result from IIC;
   int readouts[nValCnt];
-  ReadAccGyr(readouts); //读出测量值
+  int dist=Get_LidarDatafromIIC(0x10);
   getAltidata();
+  ReadAccGyr(readouts);
   float realVals[7];
-  Rectify(readouts, realVals); //根据校准的偏移量进行纠正
-  //计算加速度向量的模长，均以g为单位
-  analogWrite(13,4);
-  
+  Rectify(readouts, realVals);
   float fNorm = sqrt(realVals[0] * realVals[0] + realVals[1] * realVals[1] + realVals[2] * realVals[2]);
-  float fRoll = GetRoll(realVals, fNorm); //计算Roll角
+  float fRoll = GetRoll(realVals, fNorm);
   if (realVals[1] > 0) {
-    fRoll = -fRoll;
+  fRoll = -fRoll;
   }
-  float fPitch = GetPitch(realVals, fNorm); //计算Pitch角
+  float fPitch = GetPitch(realVals, fNorm);
   if (realVals[0] < 0) {
-    fPitch = -fPitch;
+  fPitch = -fPitch;
   }
-  //计算两次测量的时间间隔dt，以秒为单位
+
   unsigned long nCurTime = micros();
   float dt = (double)(nCurTime - nLastTime) / 1000000.0;
-  //对Roll角和Pitch角进行卡尔曼滤波
+
   float fNewRoll = kalmanRoll.getAngle(fRoll, realVals[4], dt);
   float fNewPitch = kalmanPitch.getAngle(fPitch, realVals[5], dt);
-//  float fNewdist = Kalmandist;
-  //跟据滤波值计算角度速
+
   float fRollRate = (fNewRoll - fLastRoll) / dt;
   float fPitchRate = (fNewPitch - fLastPitch) / dt;
-  //float f=kalmanRoll;
-  
- //更新Roll角和Pitch角
+
   fLastRoll = fNewRoll;
   fLastPitch = fNewPitch;
-  //更新本次测的时间
   nLastTime = nCurTime;
-  //向串口打印输出Roll角和Pitch角，运行时在Arduino的串口监视器中查看
+
+  //打印数据
+  /*Serial.print(realVals[0]*9.81);
+  Serial.print(" ");
+  Serial.print(realVals[1]*9.81);
+  Serial.print(" ");
+  Serial.println(realVals[2]*9.81);
   Serial.print("Roll:");
   Serial.print(fNewRoll); Serial.print('(');
   Serial.print(fRollRate); Serial.print("),\tPitch:");
   Serial.print(fNewPitch); Serial.print('(');
   Serial.print(fPitchRate); Serial.print(")");
-  Serial.print("temp:");Serial.println(readouts[6]);
-  delay(10);
-}
+  Serial.print("temp:");Serial.println(readouts[6]);*/
 
+  //发送数据
+  //发送数据
+  float frealVal0 = realVals[0]*9.81;
+  float frealVal1 = realVals[1]*9.81;
+  float frealVal2 = realVals[2]*9.81;
+
+  payload[0] = 0xFE;
+  memcpy(&payload[1], &frealVal0, sizeof(float));
+  memcpy(&payload[5], &frealVal1, sizeof(float));
+  memcpy(&payload[9], &frealVal2, sizeof(float));
+  memcpy(&payload[13], &fNewRoll, sizeof(float));
+  memcpy(&payload[17], &fRollRate, sizeof(float));
+  memcpy(&payload[21], &fNewPitch, sizeof(float));
+  memcpy(&payload[25], &fPitchRate, sizeof(float));
+  memcpy(&payload[29], &readouts[6], sizeof(int));
+  memcpy(&payload[33], &dist, sizeof(int));
+  payload[35] = 0xEF;
+  NRF24L01sender();
+  delay(10);  
+}
